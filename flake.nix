4baa125aa9728a0374c38b71834ebc19aa2a6dd6
Desmond Naranjo <itsdesmond@hey.com>
2026-04-18 21:20:38 -0500

    Refactor flake packages into pkgs hierarchy

--- flake.nix
+++ flake.nix
@@ -28,27 +28,12 @@
               config.allowUnfree = true;
             };
             python = pkgs.python312;
-            overrides.python3Packages = python.pkgs;
-            ocpCadViewerVersion = self.outputs.packages.${system}.ocp-vscode.version;
-            ocpCadViewer = pkgs.vscode-utils.buildVscodeExtension {
-              pname = "ocp-cad-viewer";
-              version = ocpCadViewerVersion;
-              vscodeExtPublisher = "bernhard-42";
-              vscodeExtName = "ocp-cad-viewer";
-              vscodeExtUniqueId = "bernhard-42.ocp-cad-viewer";
-              src = pkgs.fetchurl {
-                url = "https://github.com/bernhard-42/vscode-ocp-cad-viewer/releases/download/v${ocpCadViewerVersion}/ocp-cad-viewer-${ocpCadViewerVersion}.vsix";
-                hash = "sha256-RkG3Hr4GFiHoeuz4s93lU2NS72aDaEH/P3fYYyfKYSo=";
+            mkPythonEnv =
+              pythonLibs:
+              python.buildEnv.override {
+                extraLibs = pythonLibs python.pkgs;
+                ignoreCollisions = true;
               };
-            };
-            baseEditorExtensions = [
-              pkgs.vscode-extensions.ms-python.python
-              ocpCadViewer
-            ];
-            shellScope = {
-              inherit pkgs python system;
-              packages = self.outputs.packages.${system};
-            };
             resolveShellValue =
               value:
               if builtins.isFunction value then
@@ -71,8 +56,8 @@
               {
                 pythonLibs ? (
                   ps: [
-                    self.outputs.packages.${system}.cadquery
-                    self.outputs.packages.${system}.ocp-vscode
+                    packages.cadquery
+                    packages.ocp-vscode
                     ps.pip
                   ]
                 ),
@@ -87,10 +72,7 @@
                 resolvedExtraPackages = resolveShellValue extraPackages;
                 resolvedSettings = resolveShellValue settings;
                 resolvedShellHook = resolveShellValue shellHook;
-                pythonEnv = python.buildEnv.override {
-                  extraLibs = pythonLibs python.pkgs;
-                  ignoreCollisions = true;
-                };
+                pythonEnv = mkPythonEnv pythonLibs;
                 workspaceSettings = pkgs.writeText "settings.json" (builtins.toJSON (
                   {
                     "python.defaultInterpreterPath" = "${pythonEnv}/bin/python";
@@ -110,119 +92,38 @@
                   chmod -R u+w .vscode
                 '' + resolvedShellHook;
               };
-          in
-          {
-            lib = {
-              inherit mkCqcode mkWorkspaceShell;
-              mkCqcodeShell = mkWorkspaceShell;
+            packages = (import ./pkgs {
+              inherit pkgs python mkPythonEnv;
+            }) // {
+              cqcode = mkCqcode { };
             };
-            packages = {
-          default = self.outputs.packages.${system}.cadquery;
-          cadquery-env = python.buildEnv.override {
-            extraLibs = [ self.outputs.packages.${system}.cadquery ];
-            ignoreCollisions = true;
-          };
-          cadquery = pkgs.callPackage ./cadquery (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system})
-                multimethod
-                ocp
-                runtype
-                trame
-                trame-vtk
-                trame-components
-                trame-vuetify
-                ;
-            }
-          );
-          vtk = pkgs.callPackage ./vtk overrides;
-          ocp = pkgs.callPackage ./ocp (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) vtk;
-              inherit (pkgs) darwin;
-            }
-          );
-          multimethod = pkgs.callPackage ./multimethod overrides;
-          runtype = pkgs.callPackage ./runtype overrides;
-          lib3mf = pkgs.callPackage ./lib3mf overrides;
-          trianglesolver = pkgs.callPackage ./trianglesolver overrides;
-          trame-common = pkgs.callPackage ./trame-common overrides;
-          trame-client = pkgs.callPackage ./trame-client (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) trame-common;
-            }
-          );
-          trame-server = pkgs.callPackage ./trame-server overrides;
-          trame = pkgs.callPackage ./trame (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) trame-server trame-client trame-common;
-            }
-          );
-          trame-vtk = pkgs.callPackage ./trame-vtk (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) trame-client;
-            }
-          );
-          trame-components = pkgs.callPackage ./trame-components (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) trame-client;
-            }
-          );
-          trame-vuetify = pkgs.callPackage ./trame-vuetify (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) trame-client;
-            }
-          );
-          ocpsvg = pkgs.callPackage ./ocpsvg (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) ocp;
-            }
-          );
-          ocp-gordon = pkgs.callPackage ./ocp-gordon (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) ocp;
-            }
-          );
-          build123d = pkgs.callPackage ./build123d (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) ocp ocpsvg lib3mf ocp-gordon trianglesolver;
-            }
-          );
-          ocp-tessellate = pkgs.callPackage ./ocp-tessellate (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) ocp;
-            }
-          );
-          ocp-vscode = pkgs.callPackage ./ocp-vscode (
-            overrides
-            // {
-              inherit (self.outputs.packages.${system}) ocp-tessellate;
-          }
-          );
-          cqcode = mkCqcode { };
+            ocpCadViewerVersion = packages.ocp-vscode.version;
+            ocpCadViewer = pkgs.vscode-utils.buildVscodeExtension {
+              pname = "ocp-cad-viewer";
+              version = ocpCadViewerVersion;
+              vscodeExtPublisher = "bernhard-42";
+              vscodeExtName = "ocp-cad-viewer";
+              vscodeExtUniqueId = "bernhard-42.ocp-cad-viewer";
+              src = pkgs.fetchurl {
+                url = "https://github.com/bernhard-42/vscode-ocp-cad-viewer/releases/download/v${ocpCadViewerVersion}/ocp-cad-viewer-${ocpCadViewerVersion}.vsix";
+                hash = "sha256-RkG3Hr4GFiHoeuz4s93lU2NS72aDaEH/P3fYYyfKYSo=";
+              };
+            };
+            baseEditorExtensions = [
+              pkgs.vscode-extensions.ms-python.python
+              ocpCadViewer
+            ];
+            shellScope = {
+              inherit pkgs python system packages;
             };
             devShells =
               let
-                defaultPythonEnv = python.buildEnv.override {
-                  extraLibs = (ps: [
-                    self.outputs.packages.${system}.cadquery
-                    self.outputs.packages.${system}.build123d
-                    self.outputs.packages.${system}.ocp-vscode
-                    ps.pip
-                  ]) python.pkgs;
-                  ignoreCollisions = true;
-                };
+                defaultPythonEnv = mkPythonEnv (ps: [
+                  packages.cadquery
+                  packages.build123d
+                  packages.ocp-vscode
+                  ps.pip
+                ]);
               in
               {
                 default = pkgs.mkShell {
@@ -232,6 +133,13 @@
                 };
                 cqcode = mkWorkspaceShell { };
               };
+          in
+          {
+            lib = {
+              inherit mkCqcode mkWorkspaceShell;
+              mkCqcodeShell = mkWorkspaceShell;
+            };
+            inherit packages devShells;
           }
         );
     in
---
+++ pkgs/default.nix
@@ -0,0 +1,41 @@
+{ pkgs, python, mkPythonEnv }:
+let
+  overrides.python3Packages = python.pkgs;
+  scope = pkgs.lib.makeScope pkgs.newScope (self: {
+    default = self.cadquery;
+    cadquery-env = mkPythonEnv (ps: [ self.cadquery ]);
+    vtk = self.callPackage ./vtk overrides;
+    multimethod = self.callPackage ./multimethod overrides;
+    runtype = self.callPackage ./runtype overrides;
+    lib3mf = self.callPackage ./lib3mf overrides;
+    trianglesolver = self.callPackage ./trianglesolver overrides;
+    trame-common = self.callPackage ./trame-common overrides;
+    trame-server = self.callPackage ./trame-server overrides;
+
+    ocp = self.callPackage ./ocp (
+      overrides
+      // {
+        inherit (pkgs) darwin;
+      }
+    );
+
+    trame-client = self.callPackage ./trame-client overrides;
+    trame = self.callPackage ./trame overrides;
+    trame-vtk = self.callPackage ./trame-vtk overrides;
+    trame-components = self.callPackage ./trame-components overrides;
+    trame-vuetify = self.callPackage ./trame-vuetify overrides;
+
+    ocpsvg = self.callPackage ./ocpsvg overrides;
+    ocp-gordon = self.callPackage ./ocp-gordon overrides;
+    build123d = self.callPackage ./build123d overrides;
+    ocp-tessellate = self.callPackage ./ocp-tessellate overrides;
+    ocp-vscode = self.callPackage ./ocp-vscode overrides;
+    cadquery = self.callPackage ./cadquery overrides;
+  });
+in
+pkgs.lib.removeAttrs scope [
+  "newScope"
+  "callPackage"
+  "overrideScope"
+  "packages"
+]
